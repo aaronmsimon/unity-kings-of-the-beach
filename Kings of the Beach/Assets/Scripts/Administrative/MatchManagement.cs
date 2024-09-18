a95f781ade003abbf6a64292853c48021bdbdd8f
aaronmsimon <81662612+aaronmsimon@users.noreply.github.com>
2024-09-17 19:02:13 -0700

    Match Management (state, v1)

--- Kings of the Beach/Assets/Scripts/Administrative/MatchManagement.cs
+++ Kings of the Beach/Assets/Scripts/Administrative/MatchManagement.cs
@@ -1,6 +1,25 @@
 using UnityEngine;
+using RoboRyanTron.Unite2017.Events;
+
+public enum MatchState {
+    Serve,
+    BallInPlay,
+    DeadBall
+}
 
 public class MatchManagement : MonoBehaviour
 {
+    [Header("Match Data Scriptable Object")]
     [SerializeField] private MatchSO matchData;
+
+    [Header("Game Events")]
+    [SerializeField] private GameEvent GameStateChangedEvent;
+
+    private void SetMatchState(MatchState newState) {
+        if (newState == matchData.State) return;
+
+        matchData.State = newState;
+
+        GameStateChangedEvent.Raise();
+    }
 }
--- Kings of the Beach/Assets/Scripts/Administrative/MatchSO.cs
+++ Kings of the Beach/Assets/Scripts/Administrative/MatchSO.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Match", menuName = "Game/Match Data")]
 public class MatchSO : ScriptableObject
 {
+    [Header("Game State")]
+    [SerializeField] private MatchState matchState;
+
     [Header("Service")]
     [SerializeField] private SkillsSO servicePlayer;
 
@@ -12,6 +15,14 @@ public class MatchSO : ScriptableObject
         }
         set {
             servicePlayer = value;
+        }
+    }
+    public MatchState State {
+        get {
+            return matchState;
+        }
+        set {
+            matchState = value;
         }
     }
 }
