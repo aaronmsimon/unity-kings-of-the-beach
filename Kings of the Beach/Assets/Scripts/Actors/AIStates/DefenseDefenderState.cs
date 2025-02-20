5a9e63a53ac00be5e9c2d20531bcb2b396b11a7e
aaronmsimon <81662612+aaronmsimon@users.noreply.github.com>
2025-02-20 11:33:04 -0800

    Server AI Teammate shouldn't chase the ball when it won't be served over net (72t1fVNM)

--- Kings of the Beach/Assets/Scripts/Actors/AIStates/DefenseDefenderState.cs
+++ Kings of the Beach/Assets/Scripts/Actors/AIStates/DefenseDefenderState.cs
@@ -8,12 +8,14 @@ namespace KotB.StatePattern.AIStates
         public DefenseDefenderState(AI ai) : base(ai) { }
         
         private Vector3 targetPos;
-        float halfCourtSide;
+        private float halfCourtSide;
 
+        private float targetProximity = 0.1f;
         private float defensePos = 6;
         private float distToGiveUp = 1;
 
         public override void Enter() {
+            targetPos = new Vector3(defensePos * ai.CourtSide, ai.transform.position.y, ai.transform.position.z);
             halfCourtSide = ai.CourtSideLength / 2;
             
             ai.BallInfo.TargetSet += OnTargetSet;
@@ -25,6 +27,9 @@ namespace KotB.StatePattern.AIStates
 
         public override void Update() {
             ai.TargetPos = targetPos;
+            if (Vector3.SqrMagnitude(ai.transform.position - targetPos) < targetProximity * targetProximity) {
+                ai.FaceOpponent();
+            }
         }
 
         private void OnTargetSet() {
